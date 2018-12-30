ff4ae7d9a4c709fbd3c84f0cf08e29f3009f3480
Radhi Fadlillah <m.radhi.f@gmail.com>
2018-12-30 21:05:37 +0700

    Move import path to root of $GOPATH

--- main.go
+++ main.go
@@ -3,11 +3,9 @@ package main
 import (
 	"os"
 
-	_ "github.com/RadhiFadlillah/qamel-hn/backend"
+	_ "qamel-hn/backend"
 )
 
 func main() {
-	// getStories("top")
-	// return
 	runQtApp(len(os.Args), os.Args)
 }
