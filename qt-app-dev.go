ad49ca03bd7af4e67f4382ad6704dd70fad87693
Radhi Fadlillah <m.radhi.f@gmail.com>
2019-02-05 10:59:57 +0700

    Simplify file watcher

--- qt-app-dev.go
+++ qt-app-dev.go
@@ -4,23 +4,13 @@ package main
 
 import (
 	"os"
-	fp "path/filepath"
-	"strings"
 
 	"github.com/RadhiFadlillah/qamel"
-	"github.com/fsnotify/fsnotify"
 	"github.com/sirupsen/logrus"
 )
 
 func runQtApp(argc int, argv []string) {
 	logrus.Println("DEV MODE")
-
-	// Create watcher for QML dir
-	watcher, err := createWatcher("res")
-	if err != nil {
-		logrus.Fatalln("Failed to create file watcher:", err)
-	}
-	defer watcher.Close()
 
 	// Create QT app
 	app := qamel.NewApplication(len(os.Args), os.Args)
@@ -35,42 +25,9 @@ func runQtApp(argc int, argv []string) {
 	view.SetWidth(800)
 	view.ShowMaximized()
 
-	// If files in dir changed, update view
-	go func() {
-		for {
-			select {
-			case event := <-watcher.Events:
-				fName := event.Name
-				if fp.Ext(fName) == ".qmlc" || strings.Contains(fName, ".qmlc.") {
-					continue
-				}
-
-				logrus.Printf("%s: %s", event.Op.String(), fName)
-				view.Reload()
-			case err := <-watcher.Errors:
-				if err != nil {
-					logrus.Errorln("Watcher error:", err)
-				}
-			}
-		}
-	}()
+	// Watch change in QML dir
+	go view.WatchResourceDir("res")
 
 	// Exec app
 	app.Exec()
-}
-
-func createWatcher(dir string) (*fsnotify.Watcher, error) {
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		return nil, err
-	}
-
-	fp.Walk(dir, func(path string, info os.FileInfo, err error) error {
-		if info.IsDir() {
-			return watcher.Add(path)
-		}
-		return nil
-	})
-
-	return watcher, nil
 }
