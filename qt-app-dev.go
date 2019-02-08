156f5ac17f112f5c73a1e796cbb0ce59807f591f
Radhi Fadlillah <m.radhi.f@gmail.com>
2019-02-08 17:20:59 +0700

    Minor update

--- qt-app-dev.go
+++ qt-app-dev.go
@@ -3,7 +3,9 @@
 package main
 
 import (
+	"go/build"
 	"os"
+	fp "path/filepath"
 
 	"github.com/RadhiFadlillah/qamel"
 	"github.com/sirupsen/logrus"
@@ -17,16 +19,20 @@ func runQtApp(argc int, argv []string) {
 	app.SetApplicationDisplayName("Qamel HN")
 	app.SetWindowIcon(":/res/icon.png")
 
+	// Define path to resource directory
+	gopath := build.Default.GOPATH
+	resDir := fp.Join(gopath, "src", "qamel-hn", "res")
+
 	// Create viewer
 	view := qamel.NewViewer()
-	view.SetSource("res/main.qml")
+	view.SetSource(fp.Join(resDir, "main.qml"))
 	view.SetResizeMode(qamel.SizeRootObjectToView)
 	view.SetHeight(600)
 	view.SetWidth(800)
 	view.ShowMaximized()
 
-	// Watch change in QML dir
-	go view.WatchResourceDir("res")
+	// Watch change in resource dir
+	go view.WatchResourceDir(resDir)
 
 	// Exec app
 	app.Exec()
